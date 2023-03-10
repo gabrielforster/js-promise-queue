93c3786bf21f11c43edcd7cd983c6793e16f155e
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-03-10 16:18:41 -0300

    add basic server

---
+++ http-server-withtout-queue.js
@@ -0,0 +1,19 @@
+const http = require("http")
+
+function runBigProcess(){
+	for(let i = 0; i < 500000;i++){
+		console.log(i)
+	}
+	return;
+}
+
+http.createServer(async(req, res) => {
+	if(req.url === "/bomb"){
+		const start = new Date();
+		runBigProcess();
+		console.log(`The process took ${new Date() - start}ms`)
+		return res.end("BOMBED")
+	}
+
+	res.end("OK")
+}).listen(5000)
